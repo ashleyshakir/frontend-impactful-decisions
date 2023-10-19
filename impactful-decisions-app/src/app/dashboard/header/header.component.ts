babd521f31d83e1c22c47434919e367e6716ec6d
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 16:46:34 -0400

    day and date showing up on dashboard

--- impactful-decisions-app/src/app/dashboard/header/header.component.ts
+++ impactful-decisions-app/src/app/dashboard/header/header.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faBell, faHouse, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  faHouse = faHouse;
+  faAngleDown = faAngleDown;
+  faBell = faBell;
 
+  currentDate: string | null = null;
+  currentDay: string | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const date = new Date();
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+
+    this.currentDate = date.toLocaleDateString('en-US', options);
+    this.currentDay = date.toLocaleDateString('en-US', { weekday: 'long' });
+    
+  }
 }
