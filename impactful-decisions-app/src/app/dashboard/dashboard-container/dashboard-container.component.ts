711d961d7ac1bb6af386ae06ee0451e813c33e46
ashleyshakir <ashleymshakir@gmail.com>
2023-10-19 16:11:10 -0400

    Logout method added and AuthGuard tested

--- impactful-decisions-app/src/app/dashboard/dashboard-container/dashboard-container.component.ts
+++ impactful-decisions-app/src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard-container',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-container.component.scss']
 })
 export class DashboardContainerComponent {
+
+  constructor(private authService: AuthService) { }
+
+  logout(): void {
+    this.authService.logout();
+  }
 
 }
