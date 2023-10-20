d56c2e8bb3a4bfaa9f2b92038f8560e078e8379f
ashleyshakir <ashleymshakir@gmail.com>
2023-10-20 09:54:51 -0400

    styling for recent decisions added

--- impactful-decisions-app/src/app/dashboard-components/main-components/recent-decisions/recent-decisions.component.ts
+++ impactful-decisions-app/src/app/dashboard-components/main-components/recent-decisions/recent-decisions.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Decision } from 'src/app/models/decsion.model';
 import { DecisionService } from'src/app/services/decision.service';
+import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-recent-decisions',
@@ -10,8 +11,11 @@ import { DecisionService } from'src/app/services/decision.service';
   styleUrls: ['./recent-decisions.component.scss']
 })
 export class RecentDecisionsComponent implements OnInit {
+  faPencil = faPencil;
+  faTrashCan = faTrashCan;
 
-  decisions: Decision[] = [];
+  allDecisions: Decision[] = [];
+  recentDecisions: Decision[] = [];
   
 
   constructor(private route: ActivatedRoute, private router: Router, private decisionService : DecisionService) { }
@@ -20,9 +24,10 @@ export class RecentDecisionsComponent implements OnInit {
 
     this.decisionService.getDecisions().subscribe(
       (response) => {
-        this.decisions = response.data.sort((a, b) => {
+        this.allDecisions = response.data.sort((a, b) => {
           return new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime();
         });
+        this.recentDecisions = this.allDecisions.slice(0, 2);
       },
       (error) => {
         console.log("Error: ", error);
